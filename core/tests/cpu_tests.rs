353c9627896b28dad34408ac9641aceaab633db3
Henry Beberman <henry.beberman@microsoft.com>
2025-10-27 00:31:39 -0700

    cpu: implement rl_r8

--- core/tests/cpu_tests.rs
+++ core/tests/cpu_tests.rs
@@ -1415,13 +1415,18 @@ SkipIncA:
 
     // {{{ test rl_r8
     #[test]
-    #[ignore = "TODO"]
     fn execute_rl_r8() {
         const ROM: &[u8] = gbasm! {r#"
+  ld hl, 0xC000
+  ld b, 0x33
+  ld [hl], b
+  rl [hl]
+  rl b
         "#};
         let mut cpu = Cpu::init_dmg(ROM);
         cpu.mtick(200);
-        assert_hex_eq!(cpu.a(), 0x00);
+        assert_hex_eq!(cpu.b(), 0x66);
+        assert_hex_eq!(cpu.mem_dbg_read(0xC000), 0x67);
     }
     // }}}
 
